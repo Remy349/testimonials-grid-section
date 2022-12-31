454c60d6e7fb11091b9e6daed1cb76cc844d0580
Remy349 <santoraga15@gmail.com>
2022-12-31 14:55:26 -0600

    Refactor the code completed, need to add responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,70 @@
 import React from 'react'
 import { testimonials } from './testimonials'
+import {
+  CardHeader,
+  CardTestimonial,
+  CardText,
+} from './components/CardComponents'
+
+import patternQuotation from './assets/svg/bg-pattern-quotation.svg'
 
 function App() {
   return (
     <main className='font-barlow mx-6 text-white grid gap-6'>
-      <figure className='card1 shadow-md rounded-lg p-8'>
-        <div className='flex items-center gap-x-4 mb-4'>
-          <img
-            src={testimonials.testimonialOne.img}
-            className='rounded-full w-9 h-9'
-            alt='Avatar image'
-          />
-          <figcaption>
-            <h3>{testimonials.testimonialOne.name}</h3>
-            <p className='opacity-50'>Verified Graduate</p>
-          </figcaption>
-        </div>
-        <p className='font-semibold mb-4'>{testimonials.testimonialOne.text}</p>
-        <blockquote>
-          <p className='opacity-70'>
-            {testimonials.testimonialOne.testimonial}
-          </p>
-        </blockquote>
+      <figure className='card1 shadow-md rounded-lg relative z-10 p-8'>
+        <img
+          className='absolute top-0 right-8 w-28 -z-10'
+          src={patternQuotation}
+          alt='Background svg'
+        />
+        <CardHeader
+          img={testimonials.testimonialOne.img}
+          name={testimonials.testimonialOne.name}
+        />
+        <CardText text={testimonials.testimonialOne.text} />
+        <CardTestimonial
+          testimonial={testimonials.testimonialOne.testimonial}
+        />
       </figure>
       <figure className='card2 shadow-md rounded-lg p-8'>
-        <div className='flex items-center gap-x-4 mb-4'>
-          <img
-            src={testimonials.testimonialTwo.img}
-            className='rounded-full w-9 h-9'
-            alt='Avatar image'
-          />
-          <figcaption>
-            <h3>{testimonials.testimonialTwo.name}</h3>
-            <p className='opacity-50'>Verified Graduate</p>
-          </figcaption>
-        </div>
-        <p className='font-semibold mb-4'>{testimonials.testimonialTwo.text}</p>
-        <blockquote>
-          <p className='opacity-70'>
-            {testimonials.testimonialTwo.testimonial}
-          </p>
-        </blockquote>
+        <CardHeader
+          img={testimonials.testimonialTwo.img}
+          name={testimonials.testimonialTwo.name}
+        />
+        <CardText text={testimonials.testimonialTwo.text} />
+        <CardTestimonial
+          testimonial={testimonials.testimonialTwo.testimonial}
+        />
       </figure>
       <figure className='card3 shadow-md rounded-lg p-8'>
-        <div className='flex items-center gap-x-4 mb-4'>
-          <img
-            src={testimonials.testimonialThree.img}
-            className='rounded-full w-9 h-9'
-            alt='Avatar image'
-          />
-          <figcaption>
-            <h3>{testimonials.testimonialThree.name}</h3>
-            <p className='opacity-50'>Verified Graduate</p>
-          </figcaption>
-        </div>
-        <p className='font-semibold mb-4'>
-          {testimonials.testimonialThree.text}
-        </p>
-        <blockquote>
-          <p className='opacity-70'>
-            {testimonials.testimonialThree.testimonial}
-          </p>
-        </blockquote>
+        <CardHeader
+          img={testimonials.testimonialThree.img}
+          name={testimonials.testimonialThree.name}
+        />
+        <CardText text={testimonials.testimonialThree.text} />
+        <CardTestimonial
+          testimonial={testimonials.testimonialThree.testimonial}
+        />
       </figure>
       <figure className='card4 shadow-md rounded-lg p-8'>
-        <div className='flex items-center gap-x-4 mb-4'>
-          <img
-            src={testimonials.testimonialFour.img}
-            className='rounded-full w-9 h-9'
-            alt='Avatar image'
-          />
-          <figcaption>
-            <h3>{testimonials.testimonialFour.name}</h3>
-            <p className='opacity-50'>Verified Graduate</p>
-          </figcaption>
-        </div>
-        <p className='font-semibold mb-4'>
-          {testimonials.testimonialFour.text}
-        </p>
-        <blockquote>
-          <p className='opacity-70'>
-            {testimonials.testimonialFour.testimonial}
-          </p>
-        </blockquote>
+        <CardHeader
+          img={testimonials.testimonialFour.img}
+          name={testimonials.testimonialFour.name}
+        />
+        <CardText text={testimonials.testimonialFour.text} />
+        <CardTestimonial
+          testimonial={testimonials.testimonialFour.testimonial}
+        />
       </figure>
       <figure className='card5 shadow-md rounded-lg p-8'>
-        <div className='flex items-center gap-x-4 mb-4'>
-          <img
-            src={testimonials.testimonialFive.img}
-            className='rounded-full w-9 h-9'
-            alt='Avatar image'
-          />
-          <figcaption>
-            <h3>{testimonials.testimonialFive.name}</h3>
-            <p className='opacity-50'>Verified Graduate</p>
-          </figcaption>
-        </div>
-        <p className='font-semibold mb-4'>
-          {testimonials.testimonialFive.text}
-        </p>
-        <blockquote>
-          <p className='opacity-70'>
-            {testimonials.testimonialFive.testimonial}
-          </p>
-        </blockquote>
+        <CardHeader
+          img={testimonials.testimonialFive.img}
+          name={testimonials.testimonialFive.name}
+        />
+        <CardText text={testimonials.testimonialFive.text} />
+        <CardTestimonial
+          testimonial={testimonials.testimonialFive.testimonial}
+        />
       </figure>
     </main>
   )
